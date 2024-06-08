11e142c7fd9453080f4f3f05ebea3792ee9ec090
Ahmed Attia <aatia27@gmail.com>
2024-06-08 17:50:09 +0300

    remove skeletons package

--- lib/src/widgets/loader.dart
+++ lib/src/widgets/loader.dart
@@ -3,7 +3,7 @@ import 'package:mwidgets/src/consts/jsons.dart';
 import 'package:mwidgets/src/extensions/padding.dart';
 import 'package:mwidgets/src/extensions/radius.dart';
 import 'package:mwidgets/src/widgets/image.dart';
-import 'package:skeletons/skeletons.dart';
+// import 'package:skeletons/skeletons.dart';
 
 class Loader {
   static Widget loading({
@@ -25,16 +25,17 @@ class Loader {
     MPadding padding = const MPadding.set(horizontal: 24.0, vertical: 12.0),
     MBorderRadius? borderRadius,
   }) {
-    return Padding(
-      padding: padding,
-      child: SkeletonLine(
-        style: SkeletonLineStyle(
-          width: width,
-          height: height,
-          borderRadius: borderRadius ?? MBorderRadius.set(all: 16.0),
-        ),
-      ),
-    );
+    return SizedBox();
+    // return Padding(
+    //   padding: padding,
+    //   child: SkeletonLine(
+    //     style: SkeletonLineStyle(
+    //       width: width,
+    //       height: height,
+    //       borderRadius: borderRadius ?? MBorderRadius.set(all: 16.0),
+    //     ),
+    //   ),
+    // );
   }
 
   static Widget loadingText({
@@ -43,21 +44,22 @@ class Loader {
     int count = 1,
     MPadding padding = const MPadding.set(horizontal: 24.0, vertical: 2.0),
   }) {
-    return Padding(
-      padding: padding,
-      child: SkeletonParagraph(
-        style: SkeletonParagraphStyle(
-          lines: count,
-          spacing: 6,
-          lineStyle: SkeletonLineStyle(
-            randomLength: true,
-            height: height,
-            borderRadius: BorderRadius.circular(5.0),
-            maxLength: width,
-            minLength: width * 0.7,
-          ),
-        ),
-      ),
-    );
+    return SizedBox();
+    // return Padding(
+    //   padding: padding,
+    //   child: SkeletonParagraph(
+    //     style: SkeletonParagraphStyle(
+    //       lines: count,
+    //       spacing: 6,
+    //       lineStyle: SkeletonLineStyle(
+    //         randomLength: true,
+    //         height: height,
+    //         borderRadius: BorderRadius.circular(5.0),
+    //         maxLength: width,
+    //         minLength: width * 0.7,
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
