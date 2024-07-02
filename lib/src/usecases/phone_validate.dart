d9b670a027245ec402600ed4ba6791e5cf7246e4
ahmedattia <aatia27@gmail.com>
2024-07-02 16:44:58 +0300

    remove phone number package

--- lib/src/usecases/phone_validate.dart
+++ lib/src/usecases/phone_validate.dart
@@ -1,38 +1,38 @@
-import 'dart:async';
-
-import 'package:phone_number/phone_number.dart';
-
-class PhoneValidator {
-  final StreamController<bool> validate;
-
-  PhoneValidator(this.validate);
-
-  String countryCode = 'EG';
-
-  void serCountryCode(value) {
-    countryCode = value;
-  }
-
-  Future<PhoneNumber?> getPhone({required String phone}) async {
-    try {
-      PhoneNumber phoneNumber = await PhoneNumberUtil().parse(
-        phone,
-        regionCode: countryCode,
-      );
-      if (phoneNumber.e164 == '' ||
-          phoneNumber.type != PhoneNumberType.MOBILE) {
-        validate.sink.add(false);
-        return null;
-      }
-      validate.sink.add(true);
-      return phoneNumber;
-    } catch (e) {
-      validate.sink.add(false);
-      return null;
-    }
-  }
-
-  Future<void> close() async {
-    await validate.close();
-  }
-}
+// import 'dart:async';
+//
+// import 'package:phone_number/phone_number.dart';
+//
+// class PhoneValidator {
+//   final StreamController<bool> validate;
+//
+//   PhoneValidator(this.validate);
+//
+//   String countryCode = 'EG';
+//
+//   void serCountryCode(value) {
+//     countryCode = value;
+//   }
+//
+//   Future<PhoneNumber?> getPhone({required String phone}) async {
+//     try {
+//       PhoneNumber phoneNumber = await PhoneNumberUtil().parse(
+//         phone,
+//         regionCode: countryCode,
+//       );
+//       if (phoneNumber.e164 == '' ||
+//           phoneNumber.type != PhoneNumberType.MOBILE) {
+//         validate.sink.add(false);
+//         return null;
+//       }
+//       validate.sink.add(true);
+//       return phoneNumber;
+//     } catch (e) {
+//       validate.sink.add(false);
+//       return null;
+//     }
+//   }
+//
+//   Future<void> close() async {
+//     await validate.close();
+//   }
+// }
